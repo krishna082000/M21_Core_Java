66ec617dc970c7eec93d845fdccc5a4b90741572
Krishna <krishnabawache22@gmail.com>
2022-07-11 12:29:04 +0530

    First Commit

---
+++ Day1_GitHub/src/com/capgemini/iteo/Program1.java
@@ -0,0 +1,11 @@
+package com.capgemini.iteo;
+
+public class Program1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+
+	}
+
+}
