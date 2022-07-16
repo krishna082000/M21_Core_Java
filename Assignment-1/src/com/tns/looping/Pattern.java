3a05e328e90d8669442329b7a0c0d05d8a045c71
Krishna <krishnabawache22@gmail.com>
2022-07-17 01:22:37 +0530

    first commit

---
+++ Assignment-1/src/com/tns/looping/Pattern.java
@@ -0,0 +1,16 @@
+package com.tns.looping;
+
+public class Pattern {
+
+	public static void main(String[] args) {
+		 int a,b,n=5;
+	        for (a = 0; a < n; a++) {     
+	            for (b = 0; b <= a; b++) {
+	                
+	                System.out.print("* ");
+	            }
+	            System.out.println();
+	        }
+	    }
+	  
+	}
